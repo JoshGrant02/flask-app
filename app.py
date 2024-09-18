379e24440098a53a0922941b385b14a40fb6d75d
Joshua Grant <joshuajamesgrant02@gmail.com>
2024-09-18 18:49:41 -0500

    Add enhanced app

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from datetime import datetime
+from flask import Flask, send_file
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return "Hello MSOE"
+        return send_file('homepage.html')
+
+@app.route("/getTime")
+def get_time():
+        return f'{datetime.now()}'
+
+if __name__ == "__main__":
+        app.run()
